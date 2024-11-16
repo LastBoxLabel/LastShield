358e32c6453d66a24a0006ba046752fc23da3c40
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-11-16 19:59:26 -0300

    feat: add TokenStore interface, to help if user wants to persist tokens and JWTConfig to help creating JwtService easier

---
+++ security-jwt/src/main/java/tech/lastbox/JwtConfig.java
@@ -0,0 +1,116 @@
+package tech.lastbox;
+
+/**
+ * Configuration class for JWT (JSON Web Token) settings.
+ * This class holds the configuration details required for JWT creation,
+ * such as the algorithm, secret key, expiration time, and token store.
+ * It provides the necessary methods to retrieve these configurations.
+ */
+public class JwtConfig {
+    private final JwtAlgorithm jwtAlgorithm;
+    private final String secretKey;
+    private final String issuer;
+    private final long expirationAmount;
+    private final ExpirationTimeUnit expirationTimeUnit;
+    private final TokenStore tokenStore;
+
+    /**
+     * Constructs a JwtConfig with all necessary configurations including token store.
+     *
+     * @param jwtAlgorithm         The algorithm used for signing the JWT.
+     * @param secretKey            The secret key used for JWT signing.
+     * @param issuer               The issuer of the JWT.
+     * @param expirationAmount     The amount of time before the JWT expires.
+     * @param expirationTimeUnit   The unit of time for expiration (e.g., minutes, hours).
+     * @param tokenStore           The TokenStore for saving and retrieving token entities.
+     * @throws AlgorithmException If jwtAlgorithm or secretKey is null or empty.
+     */
+    public JwtConfig(JwtAlgorithm jwtAlgorithm, String secretKey, String issuer, long expirationAmount, ExpirationTimeUnit expirationTimeUnit, TokenStore tokenStore) {
+        if (jwtAlgorithm == null || secretKey == null || secretKey.isEmpty()) {
+            throw new AlgorithmException("JWT Algorithm and secretKey must not be null or empty");
+        }
+        this.jwtAlgorithm = jwtAlgorithm;
+        this.secretKey = secretKey;
+        this.issuer = issuer;
+        this.expirationAmount = expirationAmount;
+        this.expirationTimeUnit = expirationTimeUnit;
+        this.tokenStore = tokenStore;
+    }
+
+    /**
+     * Constructs a JwtConfig without a token store.
+     *
+     * @param jwtAlgorithm         The algorithm used for signing the JWT.
+     * @param secretKey            The secret key used for JWT signing.
+     * @param issuer               The issuer of the JWT.
+     * @param expirationAmount     The amount of time before the JWT expires.
+     * @param expirationTimeUnit   The unit of time for expiration (e.g., minutes, hours).
+     * @throws AlgorithmException If jwtAlgorithm or secretKey is null or empty.
+     */
+    public JwtConfig(JwtAlgorithm jwtAlgorithm, String secretKey, String issuer, long expirationAmount, ExpirationTimeUnit expirationTimeUnit) {
+        if (jwtAlgorithm == null || secretKey == null || secretKey.isEmpty()) {
+            throw new AlgorithmException("JWT Algorithm and secretKey must not be null or empty");
+        }
+        this.jwtAlgorithm = jwtAlgorithm;
+        this.secretKey = secretKey;
+        this.issuer = issuer;
+        this.expirationAmount = expirationAmount;
+        this.expirationTimeUnit = expirationTimeUnit;
+        this.tokenStore = null;
+    }
+
+    /**
+     * Gets the JWT signing algorithm.
+     *
+     * @return The JWT signing algorithm.
+     */
+    public JwtAlgorithm getJwtAlgorithm() {
+        return jwtAlgorithm;
+    }
+
+    /**
+     * Gets the secret key used for JWT signing.
+     *
+     * @return The secret key.
+     */
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    /**
+     * Gets the issuer of the JWT.
+     *
+     * @return The issuer of the JWT.
+     */
+    public String getIssuer() {
+        return issuer;
+    }
+
+    /**
+     * Gets the expiration time unit used for the JWT.
+     *
+     * @return The expiration time unit.
+     */
+    public long getExpirationAmount() {
+        return expirationAmount;
+    }
+
+    /**
+     * Gets the expiration time unit used for the JWT.
+     *
+     * @return The expiration time unit.
+     */
+    public ExpirationTimeUnit getExpirationTimeUnit() {
+        return expirationTimeUnit;
+    }
+
+    /**
+     * Gets the token store used for saving and retrieving token entities.
+     * If no token store is configured, this returns null.
+     *
+     * @return The token store, or null if not configured.
+     */
+    public TokenStore getTokenRepository() {
+        return tokenStore;
+    }
+}
---
+++ security-jwt/src/main/java/tech/lastbox/TokenStore.java
@@ -0,0 +1,29 @@
+package tech.lastbox;
+
+import java.util.Optional;
+
+/**
+ * Interface for storing and retrieving token entities.
+ * This interface defines methods for saving and finding token entities in the token store.
+ * It allows interaction with a storage system (e.g., a database) for token management.
+ * <p>
+ * Implementing classes should provide the logic for persisting and querying token entities
+ * from a specific data source such as a relational database, in-memory store, etc.
+ */
+public interface TokenStore {
+
+    /**
+     * Saves a token entity in the store.
+     *
+     * @param tokenEntity the token entity to be saved.
+     */
+    void save(TokenEntity tokenEntity);
+
+    /**
+     * Finds a token entity by its token string.
+     *
+     * @param token the token string of the entity to be retrieved.
+     * @return an {@link Optional} containing the found token entity, or {@link Optional#empty()} if not found.
+     */
+    Optional<TokenEntity> findById(String token);
+}
