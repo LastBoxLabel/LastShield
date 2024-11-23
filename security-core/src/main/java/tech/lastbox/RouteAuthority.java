b067ee2d8f6730f25d26effdd62ce61dd009b68d
Matheus <math.lima.m@gmail.com>
2024-11-22 22:02:53 -0300

    refact: update RouteAuthority

--- security-core/src/main/java/tech/lastbox/RouteAuthority.java
+++ security-core/src/main/java/tech/lastbox/RouteAuthority.java
@@ -1,15 +1,25 @@
 package tech.lastbox;
 
+import tech.lastbox.http.HttpMethod;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class RouteAuthority {
     private final String path;
     private final String[] roles;
+    private final List<HttpMethod> httpMethods = new ArrayList<>();
 
     public RouteAuthority(String path, String role) {
         this.path = path;
         this.roles = new String[] {role};
+    }
+
+    public RouteAuthority(String path, String role, HttpMethod... httpMethods) {
+        this.path = path;
+        this.roles = new String[] {role};
+        this.httpMethods.addAll(Arrays.asList(httpMethods));
     }
 
     public RouteAuthority(String path, List<String> roles) {
@@ -24,6 +34,8 @@ public class RouteAuthority {
     public String[] getRoles() {
         return roles;
     }
+
+    public List<HttpMethod> getHttpMethods() {return httpMethods;}
 
     @Override
     public String toString() {
