4724ee0d1472e0a853044be2fc724b59f5833704
Matheus <math.lima.m@gmail.com>
2024-11-22 22:01:51 -0300

    refact: update CorsConfig

--- security-core/src/main/java/tech/lastbox/CorsConfig.java
+++ security-core/src/main/java/tech/lastbox/CorsConfig.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -16,10 +18,14 @@ public class CorsConfig {
     private List<String> allowedMethods;
     private List<String> allowedHeaders;
     private Boolean allowCredentials;
-    private Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+    private final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public Customizer<CorsConfigurer<HttpSecurity>> configure(){
+        return cors -> corsConfigurationSource();
+    }
+
+    private void corsConfigurationSource() {
         logger.info("✅ CORS Configuration Initialized");
         CorsConfiguration configuration = new CorsConfiguration();
 
@@ -35,16 +41,21 @@ public class CorsConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
+
+    public void setAllowedOrigins(String origin) {this.allowedOrigins.add(origin);}
 
     public void setAllowedOrigins(List<String> allowedOrigins) {
         this.allowedOrigins = allowedOrigins;
     }
 
+    public void setAllowedMethods(String method) {this.allowedMethods.add(method);}
+
     public void setAllowedMethods(List<String> allowedMethods) {
         this.allowedMethods = allowedMethods;
     }
+
+    public void setAllowedHeaders(String header) {this.allowedHeaders.add(header);}
 
     public void setAllowedHeaders(List<String> allowedHeaders) {
         this.allowedHeaders = allowedHeaders;
