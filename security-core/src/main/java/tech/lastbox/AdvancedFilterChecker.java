8c4b7f3a076c2ecef72496da9cefb62aa2d09f5a
Matheus <math.lima.m@gmail.com>
2024-11-22 22:00:42 -0300

    refact: update AdvancedFilterChecker

--- security-core/src/main/java/tech/lastbox/AdvancedFilterChecker.java
+++ security-core/src/main/java/tech/lastbox/AdvancedFilterChecker.java
@@ -1,5 +1,12 @@
 package tech.lastbox;
 
+/**
+ * Utility class to check and manage the state of advanced filtering.
+ * <p>
+ * This class is used to determinate if an authenticated route has been passed
+ * and to manage the state of whether advanced filtering is active.
+ * </p>
+ */
 class AdvancedFilterChecker {
 
     private static boolean isAdvancedFiltered;
