b114acafd7d477230770d3fdd5f19b2339917b5b
Matheus <math.lima.m@gmail.com>
2024-11-22 22:03:51 -0300

    refact: update SecurityFilter

--- security-core/src/main/java/tech/lastbox/SecurityFilter.java
+++ security-core/src/main/java/tech/lastbox/SecurityFilter.java
@@ -1,21 +1,18 @@
 package tech.lastbox;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Collections;
+import java.io.IOException;
 
 
 @Configuration
@@ -35,29 +32,28 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-        if (AdvancedFilterChecker.isAdvancedFiltered()) {
-            var authorization = this.recoverToken(request);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            if (AdvancedFilterChecker.isAdvancedFiltered()) {
+                var authorization = this.recoverToken(request);
+                if (authorization == null || authorization.isEmpty()) throw new EmptyTokenException();
+                if (jwtService == null) throw new RuntimeException("JwtService not configured");
 
-            if (authorization == null || authorization.isEmpty()) throw new RuntimeException("Token is empty");
-            if (jwtService == null) throw new RuntimeException("JwtService not configured");
-            var tokenValidation = jwtService.validateToken(authorization);
-
-            var tokenOptional = tokenValidation.tokenOptional();
-            if (tokenOptional.isPresent() && tokenValidation.isValid()) {
-                try {
+                var tokenValidation = jwtService.validateToken(authorization);
+                var tokenOptional = tokenValidation.tokenOptional();
+                if (tokenOptional.isPresent() && tokenValidation.isValid()) {
                     var token = tokenOptional.get();
                     Object userEntity = securityUtil.findUserByUsername(userService, token.subject());
                     var authorities = securityUtil.getUserAuthorities(userEntity);
                     var authentication = new UsernamePasswordAuthenticationToken(userEntity, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authentication);
-                } catch (RuntimeException e) {
-                    logger.error("Error while authenticating: {}", e, e);
-                    throw e;
                 }
             }
+        } catch (RuntimeException e) {
+            logger.error("Exception in SecurityFilter: {}", e.getMessage(), e);
         }
     }
+
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
