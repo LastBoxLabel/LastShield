e6290c2f8fa0e17f4d6119018d3f70eea55e2f2f
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-11-24 15:59:49 -0300

    chore: add license to files

--- security-core/src/main/java/tech/lastbox/lastshield/security/core/AdvancedFilterChecker.java
+++ security-core/src/main/java/tech/lastbox/lastshield/security/core/AdvancedFilterChecker.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2024 LastBox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package tech.lastbox.lastshield.security.core;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @class
  * The {@code AdvancedFilterChecker} class provides utility methods to check
  * if advanced filtering is enabled and to manage a list of paths that should
  * not be filtered.
