1cf8689e1225842aec4679d92338146e6cb34f94
Matheus <math.lima.m@gmail.com>
2024-11-24 00:17:59 -0300

    doc: add HttpMethod javadoc

--- security-core/src/main/java/tech/lastbox/security/core/http/HttpMethod.java
+++ security-core/src/main/java/tech/lastbox/security/core/http/HttpMethod.java
@@ -1,5 +1,25 @@
 package tech.lastbox.security.core.http;
 
+/**
+ * Enum {@code HttpMethod} that represents HTTP methods with their respective descriptions.
+ *
+ * <p>This enum maps common HTTP methods used for interacting with web services.
+ * Each method has a description that explains its purpose and usage. HTTP methods are used
+ * to define the desired action when communicating with a server.</p>
+ *
+ * <ul>
+ *     <li>{@link HttpMethod#GET} - Retrieve data from the server.</li>
+ *     <li>{@link HttpMethod#POST} - Submit data to the server.</li>
+ *     <li>{@link HttpMethod#PUT} - Update or replace data on the server.</li>
+ *     <li>{@link HttpMethod#DELETE} - Remove data from the server.</li>
+ *     <li>{@link HttpMethod#PATCH} - Partially update data on the server.</li>
+ *     <li>{@link HttpMethod#HEAD} - Retrieve only the response headers.</li>
+ *     <li>{@link HttpMethod#OPTIONS} - Describe communication options with the server.</li>
+ *     <li>{@link HttpMethod#TRACE} - Echo the request for debugging.</li>
+ *     <li>{@link HttpMethod#CONNECT} - Establish a tunnel for communication.</li>
+ * </ul>
+
+ */
 public enum HttpMethod {
     GET("Retrieve data"),
     POST("Submit data"),
@@ -17,6 +37,11 @@ public enum HttpMethod {
         this.description = description;
     }
 
+    /**
+     * Returns the description associated with this HTTP method.
+     *
+     * @return The description of the HTTP method.
+     */
     public String getDescription() {
         return description;
     }
