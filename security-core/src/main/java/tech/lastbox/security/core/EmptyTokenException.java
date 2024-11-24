cf1abbba313692c904c8e6a597cfd1fcf0bb810f
Matheus <math.lima.m@gmail.com>
2024-11-24 00:16:26 -0300

    doc: add EmptyTokenException javadoc

--- security-core/src/main/java/tech/lastbox/security/core/EmptyTokenException.java
+++ security-core/src/main/java/tech/lastbox/security/core/EmptyTokenException.java
@@ -1,7 +1,17 @@
 package tech.lastbox.security.core;
 
+/**
+ * {@code EmptyTokenException} is a custom exception that extends {@link RuntimeException}.
+ * This exception is thrown when an expected token is empty or missing in the application context.
+ * <p>
+ * It provides a default error message, "Empty token", indicating that a required token was not provided
+ * or was empty during some operation, such as authentication or authorization.
+ * </p>
+ */
 public class EmptyTokenException extends RuntimeException {
-
+    /**
+     * Constructs a new {@code EmptyTokenException} with the default message "Empty token".
+     */
     public EmptyTokenException() {
         super("Empty token");
     }
